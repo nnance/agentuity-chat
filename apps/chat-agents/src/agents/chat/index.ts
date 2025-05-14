0e4ff9404a8038d6eeaa842ad9ce50ca1cbae7b8
Nick Nance <nance.nick@gmail.com>
2025-05-14 09:31:14 -0500

    transform helper functions

--- apps/chat-agents/src/agents/chat/index.ts
+++ apps/chat-agents/src/agents/chat/index.ts
@@ -1,11 +1,19 @@
 import type { AgentRequest } from "@agentuity/sdk";
-import { streamText } from "ai";
+import { streamText, type UIMessage } from "ai";
 import { anthropic } from "@ai-sdk/anthropic";
 
 export default async function Agent(req: AgentRequest) {
+	const {
+		messages,
+	}: {
+		id: string;
+		messages: Array<UIMessage>;
+	} = await req.data.object();
+
 	const result = streamText({
 		model: anthropic("claude-3-5-sonnet-latest"),
-		prompt: "Why is the sky blue?",
+		system: "you are a helpful assistant",
+		messages,
 	});
 
 	return result.toDataStreamResponse();
--- apps/chat/app/api/chat/route.ts
+++ apps/chat/app/api/chat/route.ts
@@ -1,6 +1,105 @@
-// import { openai } from '@ai-sdk/openai';
+import {
+  createDataStreamResponse,
+  formatDataStreamPart,
+  type UIMessage,
+} from "ai";
 
-import { createDataStreamResponse } from "ai";
+interface ChatAgentParameters {
+  url: string;
+  data: {
+    id: string;
+    messages: Array<UIMessage>;
+  };
+}
+
+function dataStreamPassthrough(dataStream: ReadableStream<Uint8Array>) {
+  return new ReadableStream({
+    start(controller) {
+      const reader = dataStream.getReader();
+      const decoder = new TextDecoder("utf-8");
+      async function read() {
+        if (reader) {
+          const { done, value } = await reader.read();
+          if (done) {
+            controller.close();
+            return { done };
+          }
+          const decodedValue = decoder.decode(value, { stream: true });
+          // const dataStreamPart = formatDataStreamPart(
+          //   "text",
+          //   decodedValue
+          // );
+          controller.enqueue(decodedValue);
+          return read();
+        }
+      }
+      return read();
+    },
+  });
+}
+
+function testStreamToDataStream(dataStream: ReadableStream<Uint8Array>) {
+  return new ReadableStream({
+    start(controller) {
+      const reader = dataStream.getReader();
+      const decoder = new TextDecoder("utf-8");
+      async function read() {
+        if (reader) {
+          const { done, value } = await reader.read();
+          if (done) {
+            controller.close();
+            return { done };
+          }
+          const decodedValue = decoder.decode(value, { stream: true });
+          const dataStreamPart = formatDataStreamPart("text", decodedValue);
+          controller.enqueue(dataStreamPart);
+          return read();
+        }
+      }
+      return read();
+    },
+  });
+}
+
+async function streamChatAgent({ url, data }: ChatAgentParameters) {
+  // Make a POST request to the external API
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.body) {
+    return dataStreamPassthrough(response.body);
+  }
+
+  // Create a new ReadableStream to handle the response body
+  return new ReadableStream({
+    start(controller) {
+      const reader = response.body?.getReader();
+      const decoder = new TextDecoder("utf-8");
+      async function read() {
+        if (reader) {
+          const { done, value } = await reader.read();
+          if (done) {
+            controller.close();
+            return { done };
+          }
+          const decodedValue = decoder.decode(value, { stream: true });
+          // const dataStreamPart = formatDataStreamPart(
+          //   "text",
+          //   decodedValue
+          // );
+          controller.enqueue(decodedValue);
+          return read();
+        }
+      }
+      return read();
+    },
+  });
+}
 
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
@@ -13,42 +112,20 @@ export async function POST(req: Request) {
       status: 200,
       statusText: "OK",
       async execute(dataStream) {
-        // Make a POST request to the external API
-        const response = await fetch(
-          "http://127.0.0.1:3500/agent_f1fe183d28ced8903776011734f76c12",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              // Add any other required headers here
-            },
-            body: JSON.stringify({ messages, id }),
-          }
-        );
-
-        // Create a new ReadableStream to handle the response body
-        const stream = new ReadableStream({
-          start(controller) {
-            const reader = response.body?.getReader();
-            const decoder = new TextDecoder("utf-8");
-            async function read() {
-              if (reader) {
-                const { done, value } = await reader.read();
-                if (done) {
-                  controller.close();
-                  return { done };
-                }
-                // Decode the chunk and enqueue it to the stream
-                const decodedValue = decoder.decode(value, { stream: true });
-                controller.enqueue(decodedValue);
-                return read();
-              }
-            }
-            return read();
+        const url =
+          "http://127.0.0.1:3500/agent_f1fe183d28ced8903776011734f76c12";
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
+          body: JSON.stringify({ messages, id }),
         });
-        // Read the stream and process the data
-        dataStream.merge(stream);
+
+        if (response.body) {
+          const stream = dataStreamPassthrough(response.body);
+          dataStream.merge(stream);
+        }
       },
     });
   } catch (error) {
